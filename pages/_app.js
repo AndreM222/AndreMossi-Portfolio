00a6edcc2f0fd48465fa0be2df017d4854cd8ad1
AndreM222 <mossiroberto0392@gmail.com>
2023-04-16 01:31:02 -0400

    fix: 🐛 deploy errors

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import theme from '../lib/theme'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 
-const Website = ({ Component, pageProps, router }) => {
+function Website({ Component, pageProps, router }) {
     return (
         <ChakraProvider theme={theme}>
             <Fonts />
