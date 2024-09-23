6fe6d1e4a8d30e8b71e4c8ecc120772b47d48d9c
Andre Mossi <mossiroberto0392@gmail.com>
2024-09-22 21:30:37 -0400

    feat(humanity): add more humanity than just work
    This is meant to showcase more about myself than about my work

--- pages/other.js
+++ pages/other.js
@@ -4,6 +4,7 @@ import Section from '../components/section'
 import Layout from '../components/layouts/article'
 import Paragraph from '../components/paragraph'
 import thumbNvim from '../public/images/others/Terminal-Preview.png'
+import thumbJapan from '../public/images/others/Japan-Byakutan.png'
 import Content from '../components/content'
 import othersLang from './assets/other.json'
 import miscLang from './assets/misc.json'
@@ -17,35 +18,51 @@ const Other = () => {
                 Content(miscLang, 'category', 'others'),
             ].join(' ')}
         >
-            <Container>
+            <Container maxW={{ base: "container.md", lg: "70%" }}>
                 <Heading as="h3">
                     {Content(miscLang, 'category', 'others')}
                 </Heading>{' '}
 
                 <Divider my={6} />
 
-                <Section delay={0.1} align='left'>
+                <Section delay={0.1}>
+                    <Heading as="h3" variant="section-title">
+                        {Content(othersLang, 'beginning', 'title')}
+                    </Heading>
                     <Paragraph>
-                        {Content(othersLang, 'description', 'content')}
+                        {Content(othersLang, 'beginning', 'content')}
                     </Paragraph>
                 </Section>
 
-                <Section delay={0.2}>
+                <Section delay={0.2} align='right'>
                     <Heading as="h3" variant="section-title">
                         {Content(othersLang, 'editor', 'title')}
                     </Heading>
                     <Paragraph>
                         {Content(othersLang, 'editor', 'description')}
                     </Paragraph>
+                    <GridItem
+                        href="https://github.com/AndreM222/Windows-Dotfiles"
+                        title={Content(othersLang, 'thumbnailEditor', 'title')}
+                        thumbnail={thumbNvim}
+                    >
+                        {Content(othersLang, 'thumbnailEditor', 'description')}
+                    </GridItem>
                 </Section>
 
                 <Section delay={0.3}>
+                    <Heading as="h3" variant="section-title">
+                        {Content(othersLang, 'culture', 'title')}
+                    </Heading>
+                    <Paragraph>
+                        {Content(othersLang, 'culture', 'content')}
+                    </Paragraph>
                     <GridItem
-                        href="https://github.com/AndreM222/Windows-Dotfiles"
-                        title={Content(othersLang, 'thumbnail', 'title')}
-                        thumbnail={thumbNvim}
+                        href="https://www.instagram.com/yakinikubyakutan/"
+                        title={Content(othersLang, 'thumbnailCulture', 'title')}
+                        thumbnail={thumbJapan}
                     >
-                        {Content(othersLang, 'thumbnail', 'description')}
+                        {Content(othersLang, 'thumbnailCulture', 'description')}
                     </GridItem>
                 </Section>
             </Container>
