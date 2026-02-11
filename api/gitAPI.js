3240aefe75566169b9199c69bc99f4d9c154b900
Andre Mossi <mossiroberto0392@gmail.com>
2026-02-11 20:15:50 +0900

    feat(git data): add more git data for better presentation

--- api/gitAPI.js
+++ api/gitAPI.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
 
 const gitApi = axios.create({
-  baseURL: "https://api.github.com/users/AndreM222"
+    baseURL: 'https://api.github.com/users/AndreM222'
 })
 
-export const fetchAPI = async (page, per_page) => {
-
+export const fetchAPI = async (page, per_page, extraParams = {}) => {
     let { data } = await gitApi.get("repos", {
         headers: { Authorization: process.env.NEXT_PUBLIC_GITHUB_TOKEN },
-        params: { per_page: per_page, page: page }
+        params: { per_page: per_page, page: page, ...extraParams }
     })
 
     return data
@@ -16,26 +15,26 @@ export const fetchAPI = async (page, per_page) => {
 
 export const fetchStats = async () => {
     const max = 100
-    let page = 1;
+    let page = 1
 
     const stats = {
         totalStars: 0,
         totalRepos: 1,
-        totalAwards: 2
+        totalAwards: 2,
+        totalForks: 0
     }
 
-    while(true){
-
+    while (true) {
         try {
             let fetchVal = await fetchAPI(page, max)
 
             fetchVal.map(item => {
                 stats.totalStars += item.stargazers_count
                 stats.totalRepos++
+                stats.totalForks += item.fork
             })
 
-
-            if(!fetchVal || fetchVal.length < max) break;
+            if (!fetchVal || fetchVal.length < max) break
 
             page++
         } catch (error) {
@@ -45,4 +44,37 @@ export const fetchStats = async () => {
     }
 
     return stats
+}
+
+export const fetchTopRepos = async () => {
+    try {
+        const { data } = await axios.get(
+            "https://api.github.com/search/repositories",
+            {
+                headers: {
+                    Authorization: process.env.NEXT_PUBLIC_GITHUB_TOKEN
+                },
+                params: {
+                    q: "user:AndreM222 fork:false",
+                    sort: "stars",
+                    order: "desc",
+                    per_page: 3
+                }
+            }
+        )
+
+        return data.items.map(repo => ({
+            name: repo.name,
+            description: repo.description,
+            stars: repo.stargazers_count,
+            url: repo.html_url,
+            language: repo.language,
+            forks: repo.forks_count,
+            licence: repo.license?.name || "None"
+        }))
+
+    } catch (error) {
+        console.log(error)
+        return []
+    }
 }
--- pages/experiences/github.js
+++ pages/experiences/github.js
@@ -1,17 +1,263 @@
-import { Container, Badge, List, ListItem } from '@chakra-ui/react'
+import {
+    Container,
+    Badge,
+    List,
+    ListItem,
+    chakra,
+    shouldForwardProp,
+    Box,
+    SimpleGrid,
+    Skeleton,
+    useColorModeValue,
+    Heading
+} from '@chakra-ui/react'
 import { Title, Meta } from '../../components/experience'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 import Content from '../../components/content'
 import CitationList from '../../components/citationList'
 import DateSetup from '../../components/dateSetup'
+import { fetchTopRepos, fetchStats } from '../../api/gitAPI'
+import NextLink from 'next/link'
+import { useEffect, useState } from 'react'
+import { FaBook, FaStar } from 'react-icons/fa'
+import { TbLicense } from 'react-icons/tb'
+import { FaCodeFork } from 'react-icons/fa6'
+import CountUp from 'react-countup'
+import { motion } from 'framer-motion'
 
 import githubLang from '../../locales/pages/experiences/github.json'
 import contentLang from '../../locales/experience-content.json'
-import experienceLang from '../../locales/pages/experience.json'
 import miscLang from '../../locales/misc.json'
+import experienceLang from '../../locales/pages/experience.json'
+
+const StyledDiv = chakra(motion.div, {
+    shouldForwardProp: prop => {
+        return shouldForwardProp(prop) || prop === 'transition'
+    }
+})
+
+const StatItem = ({ loading, icon, href, category, value, delay = 0 }) => (
+    <StyledDiv
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: delay }}
+    >
+        <Box
+            w="full"
+            bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+            borderStyle="solid"
+            borderColor={useColorModeValue('blackAlpha.800', 'whiteAlpha.500')}
+            boxShadow="lg"
+            borderWidth={2}
+            display="inline-flex"
+            gap={1}
+            h={10}
+            justifyContent="center"
+            rounded="lg"
+            alignItems="center"
+            as={NextLink}
+            href={href}
+        >
+            {icon}
+            <span>
+                {Content(experienceLang, 'category', category.toLowerCase())}
+            </span>
+            {loading ? (
+                <Skeleton h={5} w={20} rounded="lg" />
+            ) : (
+                <CountUp start={0} end={value || 0} />
+            )}
+        </Box>
+    </StyledDiv>
+)
+
+const TopRepoSecton = ({ delay = 0 }) => {
+    const [stats, setStats] = useState(null)
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(false)
+        setLoading(true)
+        fetchTopRepos()
+            .then(item => {
+                setStats(item)
+                setLoading(false)
+            })
+            .catch(error => console.log(error))
+    }, [])
+
+    return (
+        <StyledDiv
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: delay }}
+        >
+            <Box display="grid" gap={2}>
+                {stats?.map(item => {
+                    return (
+                        <Box
+                            w="full"
+                            bg={useColorModeValue(
+                                'whiteAlpha.500',
+                                'whiteAlpha.200'
+                            )}
+                            borderStyle="solid"
+                            borderColor={useColorModeValue(
+                                'blackAlpha.800',
+                                'whiteAlpha.500'
+                            )}
+                            boxShadow="lg"
+                            borderWidth={2}
+                            display="grid"
+                            rounded="lg"
+                            p={2}
+                            alignItems="center"
+                            as={NextLink}
+                            href={item.url}
+                        >
+                            <Box
+                                display="inline-flex"
+                                alignItems="center"
+                                justifyContent="space-between"
+                            >
+                                <Heading size="md" variant="repo-title">
+                                    {item.name}
+                                </Heading>
+                                <Heading
+                                    display={item.language ? '' : 'none'}
+                                    fontSize="10px"
+                                    bg={useColorModeValue(
+                                        'whiteAlpha.500',
+                                        'whiteAlpha.200'
+                                    )}
+                                    borderStyle="solid"
+                                    borderWidth={2}
+                                    borderColor={useColorModeValue(
+                                        'blackAlpha.800',
+                                        'whiteAlpha.500'
+                                    )}
+                                    p={1}
+                                    borderRadius={5}
+                                    w="fit-content"
+                                >
+                                    {loading ? (
+                                        <Skeleton h={5} w={20} rounded="lg" />
+                                    ) : (
+                                        item.language
+                                    )}
+                                </Heading>
+                            </Box>
+
+                            <Box
+                                display="flex"
+                                gap={3}
+                                fontSize="sm"
+                                flexWrap="wrap"
+                                rowGap={0}
+                            >
+                                <Box
+                                    display="inline-flex"
+                                    alignItems="center"
+                                    gap={1}
+                                >
+                                    <FaStar />
+                                    <span>
+                                        {Content(
+                                            experienceLang,
+                                            'category',
+                                            'stars'
+                                        )}
+                                        :
+                                    </span>
+                                    {loading ? (
+                                        <Skeleton h={5} w={20} rounded="lg" />
+                                    ) : (
+                                        <CountUp
+                                            start={0}
+                                            end={item.stars || 0}
+                                        />
+                                    )}
+                                </Box>
+                                <Box
+                                    display="inline-flex"
+                                    alignItems="center"
+                                    gap={1}
+                                >
+                                    <FaCodeFork />
+                                    <span>
+                                        {Content(
+                                            experienceLang,
+                                            'category',
+                                            'forks'
+                                        )}
+                                        :
+                                    </span>
+                                    {loading ? (
+                                        <Skeleton h={5} w={20} rounded="lg" />
+                                    ) : (
+                                        <CountUp
+                                            start={0}
+                                            end={item.forks || 0}
+                                        />
+                                    )}
+                                </Box>
+                                <Box
+                                    display={
+                                        item.licence === 'None'
+                                            ? 'none'
+                                            : 'inline-flex'
+                                    }
+                                    alignItems="center"
+                                    gap={1}
+                                >
+                                    <TbLicense />
+                                    <span>
+                                        {Content(
+                                            experienceLang,
+                                            'category',
+                                            'license'
+                                        )}
+                                        :
+                                    </span>
+                                    {loading ? (
+                                        <Skeleton h={5} w={20} rounded="lg" />
+                                    ) : (
+                                        item.licence
+                                    )}
+                                </Box>
+                            </Box>
+                            <Box alignItems="center" gap={1} mt={3}>
+                                {loading ? (
+                                    <Skeleton h={5} w={20} rounded="lg" />
+                                ) : (
+                                    item.description
+                                )}
+                            </Box>
+                        </Box>
+                    )
+                })}
+            </Box>
+        </StyledDiv>
+    )
+}
 
 const Github = () => {
+    const [stats, setStats] = useState(null)
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(true)
+        fetchStats()
+            .then(item => {
+                setStats(item)
+                setLoading(false)
+            })
+            .catch(error => console.log(error))
+    }, [])
+
     return (
         <Layout
             title={[
@@ -42,6 +288,40 @@ const Github = () => {
                         <CitationList list="my github" />
                     </ListItem>
                 </List>
+
+                <SimpleGrid columns={[1, 1, 3]} mb={3} gap={2}>
+                    <StatItem
+                        loading={loading}
+                        icon={<FaBook />}
+                        category="repositories"
+                        href="https://github.com/AndreM222?tab=repositories"
+                        value={stats?.totalRepos}
+                        delay={0.5}
+                    />
+
+                    <StatItem
+                        loading={loading}
+                        icon={<FaStar />}
+                        category="stars"
+                        href="https://github.com/AndreM222"
+                        value={stats?.totalStars}
+                        delay={0.2}
+                    />
+
+                    <StatItem
+                        loading={loading}
+                        icon={<FaCodeFork />}
+                        category="forks"
+                        href="https://github.com/AndreM222?tab=repositories"
+                        value={stats?.totalForks}
+                        delay={0.5}
+                    />
+                </SimpleGrid>
+
+                <Heading as="h2" fontSize="2xl" variant="section-title">
+                    {Content(githubLang, 'topRepos', 'title')}
+                </Heading>
+                <TopRepoSecton />
             </Container>
         </Layout>
     )
