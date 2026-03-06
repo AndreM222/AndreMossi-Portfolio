1113579535df4df0f25e417e62f91f0932c09b3c
Andre Mossi <mossiroberto0392@gmail.com>
2026-03-06 20:29:19 +0900

    fix(notifications ui): fix notifications ui by right parins and multiple promise

--- public/sw.js
+++ public/sw.js
@@ -2,25 +2,25 @@ self.addEventListener('push', event => {
     const data = event.data.json()
 
     event.waitUntil(
-        self.registration.showNotification(data.title, {
-            body: data.body,
-            icon: '/apple-touch-icon.png',
-            badge: '/apple-touch-icon.png',
-            vibrate: [100, 50, 100],
-            tag: 'portfolio-news',
-            data: {
-                url: data.url
-            }
-        }),
+        Promise.all([
+            self.registration.showNotification(data.title, {
+                body: data.body,
+                icon: '/apple-touch-icon.png',
+                badge: '/apple-touch-icon.png',
+                vibrate: [100, 50, 100],
+                tag: 'portfolio-news',
+                data: { url: data.url }
+            }),
 
-        self.clients.matchAll().then(clients => {
-            clients.forEach(client => {
-                client.postMessage({
-                    type: 'UNREAD_NOTIFICATION',
-                    count: 1
+            self.clients.matchAll().then(clients => {
+                clients.forEach(client => {
+                    client.postMessage({
+                        type: 'UNREAD_NOTIFICATION',
+                        count: 1
+                    })
                 })
             })
-        })
+        ])
     )
 })
 
