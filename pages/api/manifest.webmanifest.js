454bb0651e8d20ec126e24433448b8dad56b0c52
Andre Mossi <mossiroberto0392@gmail.com>
2026-03-07 12:31:37 +0900

    feat(manfiset monochrome): add monochrome hint to icons for dakr and light mode

--- pages/api/manifest.webmanifest.js
+++ pages/api/manifest.webmanifest.js
@@ -26,12 +26,14 @@ export default function handler(req, res) {
             {
                 src: '/apple-touch-icon.png',
                 sizes: '192x192',
-                type: 'image/png'
+                type: 'image/png',
+                purpose: 'maskable'
             },
             {
                 src: '/apple-touch-icon.png',
                 sizes: '512x512',
-                type: 'image/png'
+                type: 'image/png',
+                purpose: 'maskable'
             },
             {
                 src: '/apple-touch-icon.png',
