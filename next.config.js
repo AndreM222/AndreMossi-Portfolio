fed1a294ad99732b7ab62ac5565fcd78811029d5
Andre Mossi <mossiroberto0392@gmail.com>
2026-03-16 08:16:48 +0900

    fix(cache offline): fix cache offline for users who want to return with not internet

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
+const runtimeCaching = require('next-pwa/cache')
+
 const withPWA = require('next-pwa')({
     dest: 'public',
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
+
+    runtimeCaching,
+
+    cacheStartUrl: true,
+    dynamicStartUrl: true,
+
     fallbacks: {
-        document: "/_offline"
+        document: '/_offline'
     },
     buildExcludes: [/app-build-manifest\.json$/]
 })
